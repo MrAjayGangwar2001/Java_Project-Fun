d67203481d1d9a3a2ee1fa27162dbc1b6118cc74
Ajay <gangwar030@gmail.com>
2025-08-13 00:17:26 +0530

    All Books

---
+++ Library Management System/Books.java
@@ -0,0 +1,47 @@
+public class Books {
+
+    private int Book_Id;
+    private String title;
+    private String author;
+    boolean isIssued;
+
+    public Books(int book_Id, String title, String author) {
+        this.Book_Id = book_Id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    public int getBook_Id() {
+        return Book_Id;
+    }
+    public void setBook_Id(int book_Id) {
+        this.Book_Id = book_Id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public boolean isIssued() {
+        return isIssued;
+    }
+    public void setIssued(boolean issued) {
+        isIssued = issued;
+    }
+
+    public void displayBookInfo() {
+        System.out.println("Book ID: " + Book_Id);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Is Issued: " + (isIssued ? "Yes" : "No"));
+    }
+
+}
