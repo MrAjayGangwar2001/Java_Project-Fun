0b5e09b006d2c108809e4909734ece397256f3c5
Ajay <gangwar030@gmail.com>
2025-08-12 07:46:45 +0530

    Books Library

---
+++ Library Management System/Library.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class Library extends Books {
+    Scanner sc = new Scanner(System.in);
+    private Books[] books;
+    private User[] users;
+    private int bookCount;
+
+   // public Library(int book_Id, String title, String author) {
+   // super(book_Id, "title", "author");
+    public Library() {
+        super(0, "", "");
+        this.books = new Books[100];
+        this.users = new User[100];
+        this.bookCount = 0;
+
+    }
+
+    public void AddBook(int Book_Id, String title, String author) {
+        if (bookCount < books.length) {
+            books[bookCount] = new Books(Book_Id, title, author);
+            bookCount++;
+            setBook_Id(Book_Id);
+            setTitle(title);
+            setAuthor(author);
+            setIssued(false);
+            displayBookInfo();
+            // System.out.println("Book ID: " + Book_Id);
+            // System.out.println("Title: " + title);
+            // System.out.println("Author: " + author);
+            System.out.println("Book added successfully.");
+        } else {
+            System.out.println("Book list is full. Cannot add more books.");
+        }
+
+    }
+
+    public void SearchBook(String titleOrAuthor) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getTitle().equalsIgnoreCase(titleOrAuthor)
+                    || books[i].getAuthor().equalsIgnoreCase(titleOrAuthor)) {
+                books[i].displayBookInfo();
+                System.out.println("The Book " +titleOrAuthor + " are Available in the Library");
+                return;
+            }
+        } System.out.println("The book Not Available in the Library");
+    }
+
+    public void IssueBook(int bookId, int userId) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getBook_Id() == bookId && !books[i].isIssued()) {
+                books[i].setIssued(true);
+                System.out.println("Book issued successfully to User ID: " + userId);
+                return;
+              //  sc.nextInt();
+            }
+            
+        } System.out.println("Book not available or already issued.");
+    }
+
+    public void ReturnBook(int bookId) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getBook_Id() == bookId && books[i].isIssued()) {
+                books[i].setIssued(false);
+                System.out.println("Book returned successfully.");
+                return;
+            }
+        }
+        System.out.println("Book not found or not issued.");
+    }
+
+    public void RegisterUser(String userName, int userId, String userType) {
+        User newUser;
+        if (userType.equalsIgnoreCase("Student")) {
+            newUser = new Student(userName, userId);
+        } else if (userType.equalsIgnoreCase("Librarian")) {
+            newUser = new Librarian(userName, userId);
+        } else {
+            System.out.println("Invalid user type.");
+            return;
+        }
+
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] == null) {
+                users[i] = newUser;
+                System.out.println("User registered successfully.");
+                return;
+            }
+        }
+        System.out.println("User registration failed. User list is full.");
+    }
+
+    public void AllBooksDisplay() {
+        if (bookCount == 0) {
+            System.out.println("No books available in The Library.");
+        } else {
+            // System.out.println("Available Books are :");
+            for (int i = 0; i < bookCount; i++) {
+                books[i].displayBookInfo();
+                System.out.println("---------------------");
+            }
+
+        }
+    }
+
+}
